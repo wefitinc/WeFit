d978c40e59a737524323310cfa4348dcaddc0420
DelishusCake <redkast@gmail.com>
2019-06-23 14:04:03 -0700

    Begin to work on about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # Static page routing
+  get '/about', :to => 'about#index', :as => :about
   get '/professionals', :to => 'professionals#index', :as => :professionals
-
+  # Session routing
   get '/login', :to => 'sessions#new', :as => :login
   match '/auth/:provider/callback', :to => 'sessions#create', :via => [:get, :post]
   get '/auth/failure', :to => 'sessions#failure'
