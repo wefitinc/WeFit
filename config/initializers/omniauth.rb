7bcf0adbf9340563f6ff3cd2a64d483a989f6efd
DelishusCake <redkast@gmail.com>
2019-06-23 11:28:12 -0700

    Begin to add facebook login

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :facebook, "2383595921758644", "a1d8ccae87640ca34cb2e541c02d03e8"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'professionals/index'
   get 'welcome/index'
+
+  get '/login', :to => 'sessions#new', :as => :login
+  match '/auth/:provider/callback', :to => 'sessions#create', :via => [:get, :post]
+  get '/auth/failure', :to => 'sessions#failure'
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
