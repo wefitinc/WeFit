056a7719106b59fbf7d5ea9ea17fa9a8fac96ab6
DelishusCake <redkast@gmail.com>
2019-06-23 17:42:05 -0700

    Begin work on user login system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
+    auth_hash = request.env['omniauth.auth']
+    @authorization = Authorization.find_user_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+    
   end
 
   def failure
