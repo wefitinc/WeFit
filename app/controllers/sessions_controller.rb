702cbc3deb16062d5cd8dc1679c4b110281a7e98
Hunter Brady <redkast@gmail.com>
2019-07-02 20:13:25 -0700

    Login form works now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,16 +24,15 @@ class SessionsController < ApplicationController
     # If there isn't a provider, this is not an Omniauth login
     if not params[:provider] 
       # Get the user from the DB by the email
-    	@user = User.find_by_email(params[:email])
+    	user = User.find_by_email(params[:email])
       # If the user exists and the password is correct
-      if @user && @user.authenticate(params[:password])
+      if user && user.authenticate(params[:password])
         # Set the user id in the session
-        session[:user_id] = @user.id
-        # Go back home
+        session[:user_id] = user.id
         redirect_to root_url
       else
         # TODO: Flash invalid email and password or something
-        redirect_to root_url
+        redirect_to root_url, notice: "Invalid email or password"
       end
     else
       # TODO: Add ommiauth 
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/terms_of_use', :to => 'static#terms_of_use', :as => :terms_of_use
   get '/privacy_policy', :to => 'static#privacy_policy', :as => :privacy_policy
   # Session routing
+  post '/login', :to => 'sessions#create'
   post '/signup', :to => 'sessions#new'
   get '/logout', :to => 'sessions#destroy'
   get '/auth/:provider/callback', :to => 'sessions#create'
