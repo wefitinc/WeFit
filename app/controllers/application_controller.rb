b58d74e9942120342ad463541481f9c113eaea18
Hunter Brady <redkast@gmail.com>
2019-07-13 19:02:22 -0700

    Add welcome counter, fix some helper functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-
-  # Helper, allow pages to access the current user and get their information
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+  def current_user?
+    current_user != nil
+  end
 end
