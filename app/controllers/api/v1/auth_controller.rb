fba22b265a7c0144e174855bb9e01bb74c035d46
Hunter Brady <redkast@gmail.com>
2019-07-23 19:19:54 -0700

    Some cleanup

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -30,28 +30,28 @@ class Api::V1::AuthController < Api::V1::BaseController
     render json: @current_user, except: [:created_at, :updated_at, :password_digest, :reset_digest, :reset_sent_at]
   end
 
-private
-  def authorize
-    # Get the authorization token from the header
-  	header = request.headers['Authorization']
-  	header = header.split(' ').last if header
-  	begin
-      # Try and decode the token
-  		@decoded = JsonWebToken.decode(header)
-      # Get the user by the user ID from the token
-  		@current_user = User.find(@decoded[:user_id])
-      # Return an error if the user doesn't exist 
-      # NOTE: This should not happen, the token is encoded and signed with the secret key
-      # But just incase theres a server error or something
-      render json: { errors: "User not found" }, status: :not_found if @current_user == nil
-  	rescue JWT::DecodeError => e
-      # If theres an error decoding the token, respond with the error and a 401 status
-  		render json: { errors: e.message }, status: :unauthorized
-  	end 
-  end
-  def login_params
-    params.permit(
-        :email, 
-        :password)
-  end
+  private
+    def authorize
+      # Get the authorization token from the header
+    	header = request.headers['Authorization']
+    	header = header.split(' ').last if header
+    	begin
+        # Try and decode the token
+    		@decoded = JsonWebToken.decode(header)
+        # Get the user by the user ID from the token
+    		@current_user = User.find(@decoded[:user_id])
+        # Return an error if the user doesn't exist 
+        # NOTE: This should not happen, the token is encoded and signed with the secret key
+        # But just incase theres a server error or something
+        render json: { errors: "User not found" }, status: :not_found if @current_user == nil
+    	rescue JWT::DecodeError => e
+        # If theres an error decoding the token, respond with the error and a 401 status
+    		render json: { errors: e.message }, status: :unauthorized
+    	end 
+    end
+    def login_params
+      params.permit(
+          :email, 
+          :password)
+    end
 end
