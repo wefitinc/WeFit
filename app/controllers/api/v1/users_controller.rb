cea9f5c6348c6c7bf0a79887feae8926062024ec
Hunter Brady <redkast@gmail.com>
2019-07-23 19:22:02 -0700

    Actually we don't need a view at all

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   # GET /users/:id
   def show
+    render json: @user, except: [:created_at, :updated_at, :password_digest, :reset_digest, :reset_sent_at]
   end
 
   private
