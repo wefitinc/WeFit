ceba09d15da349f885ed88ae3d952e52a7816529
Hunter Brady <redkast@gmail.com>
2019-07-23 19:15:21 -0700

    Cleanup on the api views

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,9 +3,6 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   # GET /users/:id
   def show
-    # Strip out some sensitive user data and return as JSON 
-    # render json: , except: [:created_at, :updated_at, :password_digest, :reset_digest, :reset_sent_at]
-    render 'users/show'
   end
 
   private
