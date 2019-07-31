7b964272941dbd5f1a17a938e0e644f0abade8cb
Hunter Brady <redkast@gmail.com>
2019-07-31 14:39:10 -0700

    Add a not found error for the users API controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   # GET /users/:id
   def show
+    # Render the user
     render_user @user
   end
 
@@ -11,5 +12,7 @@ class Api::V1::UsersController < Api::V1::BaseController
     def set_user
       # Use find_by_hashid to not allow sequential ID lookups
       @user = User.find_by_hashid(params[:id])
+      # Return a not found error if the user doesnt exist
+      render json: { error: 'User not found' }, status: :not_found if @user.nil?
     end
 end
