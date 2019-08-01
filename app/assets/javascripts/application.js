954cfd2b86a867fdea7692cc99ceee6c74203359
Hunter Brady <redkast@gmail.com>
2019-08-01 16:31:04 -0700

    Move javascript to the proper file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,45 @@ window.fbAsyncInit = function()
 		version          : 'v3.3'
 	});
 };
+
+function toggle_password()
+{
+	var field = document.getElementById('user_password');
+	field.type = (field.type === 'password') ? "text" : "password";
+};
+
+$(document).on('turbolinks:load', function() {});
+
+$('.static.welcome.logged_in').ready(function()
+{
+	// The date we're hurdling inexorably towards
+	var to_date = new Date("Dec 20, 2019").getTime();
+	// Update the timer
+	function update_timer()
+	{
+		// Get the delta
+		var now = new Date().getTime();
+		var delta = to_date - now;
+		// Update the counter with magic math
+		$("#days").text(Math.floor(delta / (1000*60*60*24)));
+		$("#hours").text(Math.floor((delta % (1000*60*60*24)) / (1000*60*60)));
+		$("#minutes").text(Math.floor((delta % (1000*60*60)) / (1000*60)));
+		$("#seconds").text(Math.floor((delta % (1000*60)) / (1000)));
+	};
+	// Update every second
+	setInterval(update_timer, 1000);
+	// Call on load too
+	update_timer();
+});
+$('.static.welcome.logged_out').ready(function()
+{
+	// Select the first name by default
+	$("#user_first_name").select();
+
+	var today = new Date();
+	var picker = new Pikaday({
+		field: document.getElementById('user_birthdate'),
+		format: 'YYYY-MM-DD',
+		yearRange: [today.getFullYear()-100, today.getFullYear()]
+	});
+});
