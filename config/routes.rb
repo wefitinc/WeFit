16933e2aabde27a1146615fe347cda9b7e4ea518
Hunter Brady <redkast@gmail.com>
2019-07-31 18:51:30 -0700

    Cleanup the routing code

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # API
   namespace :api do
     namespace :v1 do
-      post '/auth/signup', :to => 'auth#signup'
-      post '/auth/login', :to => 'auth#login'
-      get  '/auth/check', :to => 'auth#check'
-      get  '/auth/me', :to => 'auth#me'
+      post '/auth/signup', to: 'auth#signup'
+      post '/auth/login', to: 'auth#login'
+      get  '/auth/check', to: 'auth#check'
+      get  '/auth/me', to: 'auth#me'
       
       resources :users, only: [:show]
     end
@@ -14,20 +14,20 @@ Rails.application.routes.draw do
 
   # Website
   # Static page routing
-  get '/about', :to => 'static#about', :as => :about
-  get '/signup', :to => 'static#signup', :as => :signup
-  get '/advertising', :to => 'static#advertising', :as => :advertising
-  get '/terms_of_use', :to => 'static#terms_of_use', :as => :terms_of_use
-  get '/professionals', :to => 'static#professionals', :as => :professionals
-  get '/privacy_policy', :to => 'static#privacy_policy', :as => :privacy_policy
+  get '/about', to: 'static#about'
+  get '/signup', to: 'static#signup'
+  get '/advertising', to: 'static#advertising'
+  get '/terms_of_use', to: 'static#terms_of_use'
+  get '/professionals', to: 'static#professionals'
+  get '/privacy_policy', to: 'static#privacy_policy'
   # Contact routing
-  get  '/contact', :to => 'contact#new', :as => :contact
-  post '/send_contact', :to => 'contact#create', :as => :send_contact
+  get  '/contact', to: 'contact#new'
+  post '/contact', to: 'contact#create'
   # Session routing
-  post '/login', :to => 'sessions#create'
-  get '/logout', :to => 'sessions#destroy'
-  get '/auth/:provider/callback', :to => 'sessions#create'
-  get '/auth/failure', :to => 'sessions#failure'
+  post '/login', to: 'sessions#create'
+  get  '/logout', to: 'sessions#destroy'
+  get  '/auth/:provider/callback', to: 'sessions#create'
+  get  '/auth/failure', to: 'sessions#failure'
   # User routing
   resources :users, only: [:create]
   # Password reset routing
