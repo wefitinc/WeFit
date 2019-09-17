2e0f4ecae34198852f8a7c1f5d22a288d95fc7e8
Hunter Brady <redkast@gmail.com>
2019-09-17 16:57:55 -0700

    Move activation-resend to the account settings page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,14 @@
 class UserMailer < ApplicationMailer
   def password_reset(user)
-    @user = user
-    mail to: user.email, subject: "Password reset"
+    send_user_email user, "Password reset"
   end
   def welcome(user)
-    @user = user
-    mail to: user.email, subject: "Welcome to Wefit!"
+    send_user_email user, "Welcome to Wefit!"
   end
+
+private
+	def send_user_email(user, subject)
+		@user = user
+    mail to: user.email, subject: subject
+	end
 end
