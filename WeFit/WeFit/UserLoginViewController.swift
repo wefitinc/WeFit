ad3c68647e81a085cdd3fc825345d5473c005c8a
Junior Alvarado <jralvarado1313@gmail.com>
2019-09-17 19:40:47 -0700

    added iOS screens

---
+++ WeFit/WeFit/UserLoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UserLoginViewController.swift
+//  WeFit
+//
+//  Created by Junior Alvarado on 9/17/19.
+//  Copyright © 2019 WeFit. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserLoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
