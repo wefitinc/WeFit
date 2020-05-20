800330bab261197561bac1919b666ac003f18f86
Hunter Brady <redkast@gmail.com>
2020-05-19 19:20:35 -0700

    Massive site overhaul

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,22 +1,2 @@
 class StaticController < ApplicationController
-	def terms_of_use
-	end
-	def privacy_policy
-	end
-	def advertising
-	end
-	def professionals
-	end
-	def community_guidelines
-	end
-	def cookie_policy
-	end
-	def advertising_policy
-	end
-	def branding_assets	
-	end
-	def what_is_we_fit
-	end
-	def billing_page
-	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,35 +32,14 @@ Rails.application.routes.draw do
   end
 
   # Website
-  # Static page routing
-  get '/branding_assets', to: 'static#branding_assets'
-  get '/advertising', to: 'static#advertising'
-  get '/terms_of_use', to: 'static#terms_of_use'
-  get '/privacy_policy', to: 'static#privacy_policy'
-  get '/community_guidelines', to: 'static#community_guidelines'
-  get '/cookie_policy', to: 'static#cookie_policy'
-  get 'advertising_policy', to: 'static#advertising_policy'
-  get 'what_is_we_fit', to: 'static#what_is_WeFit'
-  get 'billing_page', to: 'static#billing_page'
-  # Contact routing
-  get  '/contact', to: 'contact#new'
-  post '/contact', to: 'contact#create'
-  # Session routing
-  post '/login', to: 'sessions#create'
-  get  '/logout', to: 'sessions#destroy'
-  get  '/auth/:provider/callback', to: 'sessions#create'
-  get  '/auth/failure', to: 'sessions#failure'
   # Password reset routing
   resources :password_resets, only: [:new, :create, :edit, :update]
   # Account activation routing
   resources :activations, only: [:new, :edit]
-  # Professionals routing
-  get  '/professionals/new/', to: 'professionals#new'
-  post '/professionals', to: 'professionals#create'
-  # User routing
-  get   'account_settings', to: 'users#edit'
-  patch 'account_settings', to: 'users#update'
+  # Static pages
+  get 'about', to: 'static#about'
+  get 'advertise', to: 'static#advertise'
+  get 'professionals', to: 'static#professionals'
   # Root controller
-  post '/', to: 'users#create'
-  root 'users#new'
+  root 'static#home'
 end
