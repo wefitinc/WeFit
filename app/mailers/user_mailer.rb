5f648b602065cb4acc71fe2c8a0c23741801f449
Ankit Sharma <ankit.federer@gmail.com>
2021-06-25 16:57:13 +0530

    Adding email templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,12 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     send_user_email user, "Welcome to Wefit!"
   end
+  def feedback(user)
+    send_user_email user, "Feedback!"
+  end
+  def weekly_roundup(user)
+    send_user_email user, "Weekly Roundup!"
+  end
 
 private
 	def send_user_email(user, subject)
